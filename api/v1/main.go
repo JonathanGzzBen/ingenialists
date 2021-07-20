d5a846a8d01b2a11f9120ecf664db0a94a97ceb5
JonathanGzzBen <jonathangzzben@gmail.com>
2021-07-20 16:57:52 -0500

    Refactor server and database initialization

--- api/v1/main.go
+++ api/v1/main.go
@@ -1,17 +1,8 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
-
-	"github.com/JonathanGzzBen/ingenialists/api/v1/controllers"
 	_ "github.com/JonathanGzzBen/ingenialists/api/v1/docs"
-	"github.com/JonathanGzzBen/ingenialists/api/v1/models"
+	"github.com/JonathanGzzBen/ingenialists/api/v1/pkg"
 )
 
 // @title Ingenialists API V1
@@ -38,72 +29,6 @@ import (
 // @scope.profile Grant access to profile
 // @scope.email Grant access to email
 func main() {
-	godotenv.Load(".env")
-	// Initialize database
-	db, err := models.DB()
-	if err != nil {
-		log.Panic("could not open database file")
-	}
-	db.AutoMigrate(&models.User{})
-	db.AutoMigrate(&models.Category{})
-	db.AutoMigrate(&models.Article{})
-
-	r := gin.Default()
-
-	v1 := r.Group("/v1")
-	{
-		uc := controllers.NewUsersController(db)
-		ur := v1.Group("/users")
-		{
-			ur.GET("/", uc.GetAllUsers)
-			ur.GET("/:id", uc.GetUser)
-			ur.PUT("/:id", uc.UpdateUser)
-		}
-		ac := controllers.NewAuthController(db)
-		ar := v1.Group("/auth")
-		{
-			ar.GET("/", ac.GetCurrentUser)
-			ar.GET("/google-login", ac.LoginGoogle)
-			ar.GET("/google-callback", ac.GoogleCallback)
-		}
-		cc := controllers.NewCategoriesController(db)
-		cr := v1.Group("/categories")
-		{
-			cr.GET("/", cc.GetAllCategories)
-			cr.GET("/:id", cc.GetCategory)
-			cr.POST("/", cc.CreateCategory)
-			cr.PUT("/:id", cc.UpdateCategory)
-			cr.DELETE("/:id", cc.DeleteCategory)
-		}
-		arc := controllers.NewArticlesController(db)
-		arr := v1.Group("/articles")
-		{
-			arr.GET("/", arc.GetAllArticles)
-			arr.GET("/:id", arc.GetArticle)
-			arr.POST("/", arc.CreateArticle)
-			arr.PUT("/:id", arc.UpdateArticle)
-			arr.DELETE("/:id", arc.DeleteArticle)
-		}
-	}
-
-	// hostname is used by multiple controllers
-	// to make requests to authentication controller
-	hostname := os.Getenv("ING_HOSTNAME")
-
-	if len(hostname) == 0 {
-		panic("Environment variable ING_HOSTNAME missing")
-	}
-
-	swaggerUrl := ginSwagger.URL(hostname + "/v1/swagger/doc.json")
-	v1.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, swaggerUrl))
-
-	if os.Getenv("ING_ENVIRONMENT") == "development" {
-		port := os.Getenv("ING_PORT")
-		if len(port) == 0 {
-			panic("Environment variable ING_PORT missing")
-		}
-		r.Run(port)
-	} else {
-		r.Run()
-	}
+	api := pkg.IngenialistsAPIV1{}
+	api.Run()
 }
