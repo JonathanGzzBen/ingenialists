8908a51c367f0564435dd9b4caa9242bb067a504
JonathanGzzBen <jonathangzzben@gmail.com>
2021-07-21 17:37:22 -0500

    Add mockdata for Categories Tests and GetCategory test

--- api/v1/server/categories_test.go
+++ api/v1/server/categories_test.go
@@ -5,10 +5,20 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/JonathanGzzBen/ingenialists/api/v1/models"
 )
+
+// This data should not be modified, its purpose
+// is to be used to initialize database.
+var mockCategories = []models.Category{
+	{ID: 1231, Name: "First Category", ImageURL: "https://i.imgur.com/oCsJWt7.jpeg"},
+	{ID: 2131, Name: "Second Name", ImageURL: "https://i.imgur.com/oCsJWt7.jpeg"},
+	{ID: 56232, Name: "Third Name", ImageURL: "https://i.imgur.com/oCsJWt7.jpeg"},
+}
 
 func TestGetAllCategories(t *testing.T) {
 	e := NewTestEnvironment()
@@ -16,12 +26,7 @@ func TestGetAllCategories(t *testing.T) {
 	ts := httptest.NewServer(e.Server.Router)
 	defer ts.Close()
 
-	categoriesInDB := []models.Category{
-		{Name: "First Category"},
-		{Name: "Second Category"},
-		{Name: "Third Category"},
-	}
-	e.DB.Create(&categoriesInDB)
+	e.DB.Create(&mockCategories)
 
 	res, err := http.Get(fmt.Sprintf("%s/v1/categories", ts.URL))
 	if err != nil {
@@ -47,7 +52,47 @@ func TestGetAllCategories(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
-	if len(categoriesInDB) != len(resCategories) {
-		t.Fatalf("Expected %v, got %v", len(categoriesInDB), len(resCategories))
+	if len(mockCategories) != len(resCategories) {
+		t.Fatalf("Expected %v, got %v", len(mockCategories), len(resCategories))
 	}
+}
+
+func TestGetCategory(t *testing.T) {
+	e := NewTestEnvironment()
+	defer e.Close()
+	ts := httptest.NewServer(e.Server.Router)
+	defer ts.Close()
+
+	e.DB.Create(&mockCategories)
+	// Take second category to make sure it's finding
+	// it by the ID and not the first item in DB
+	mockCategory := mockCategories[1]
+
+	res, err := http.Get(fmt.Sprintf("%s/v1/categories/"+strconv.Itoa(int(mockCategory.ID)), ts.URL))
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, got %v", res.StatusCode)
+	}
+
+	val, ok := res.Header["Content-Type"]
+
+	if !ok {
+		t.Fatalf("Expected Content-Type header to be set")
+	}
+
+	if val[0] != "application/json; charset=utf-8" {
+		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+	}
+	var resCategory models.Category
+	err = json.NewDecoder(res.Body).Decode(&resCategory)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	if !reflect.DeepEqual(resCategory, mockCategory) {
+		t.Fatalf("Expected %v, got %v", mockCategory, resCategory)
+	}
+
 }
