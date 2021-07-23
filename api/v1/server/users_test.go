86b91323a99480eecce3c34c7047191598b846bc
JonathanGzzBen <jonathangzzben@gmail.com>
2021-07-23 12:09:38 -0500

    Add GetUser test

--- api/v1/server/users_test.go
+++ api/v1/server/users_test.go
@@ -49,3 +49,38 @@ func TestGetAllUsers(t *testing.T) {
 		t.Fatalf("Expected %v, got %v", len(mockUsers), len(resUsers))
 	}
 }
+
+func TestGetUser(t *testing.T) {
+	e := NewTestEnvironment()
+	defer e.Close()
+	ts := httptest.NewServer(e.Server.Router)
+	defer ts.Close()
+
+	e.DB.Create(&mockUsers)
+	mockUser := mockUsers[1]
+
+	res, err := http.Get(fmt.Sprintf("%s/v1/users/%d", ts.URL, mockUser.ID))
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, got %v", res.StatusCode)
+	}
+
+	val, ok := res.Header["Content-Type"]
+	if !ok {
+		t.Fatalf("Expected Content-Type header to be set")
+	}
+	if val[0] != "application/json; charset=utf-8" {
+		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+	}
+
+	var resUser models.User
+	err = json.NewDecoder(res.Body).Decode(&resUser)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	if mockUser != resUser {
+		t.Fatalf("Expected %v, got %v", mockUser, resUser)
+	}
+}
