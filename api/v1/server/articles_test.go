01262ceb1dee56b916f12ca1c0571e09edf8f60d
JonathanGzzBen <jonathangzzben@gmail.com>
2021-08-31 15:34:47 -0500

    Mock UpdateArticle

--- api/v1/server/articles_test.go
+++ api/v1/server/articles_test.go
@@ -320,34 +320,22 @@ func TestUpdateArticleAsUnauthenticatedUserReturnForbidden(t *testing.T) {
 	ts := httptest.NewServer(s.Router)
 	defer ts.Close()
 
-	c := models.Category{
-		ID:   1,
-		Name: "First category",
-	}
-
-	aToCreate := models.Article{
+	aToUpdate := models.Article{
 		ID:         0,
 		Title:      "First article",
-		CategoryID: c.ID,
+		CategoryID: 1,
 		UserID:     1,
 	}
 
-	aCreated := aToCreate
-	aCreated.ID = 1
-
 	mockArticlesRepo := &mocks.ArticlesRepository{}
-	mockArticlesRepo.On("CreateArticle", &aToCreate).Return(&aCreated, nil)
 	s.ArticlesRepo = mockArticlesRepo
-	mockCategoriesRepo := &mocks.CategoriesRepository{}
-	mockCategoriesRepo.On("GetCategory", aToCreate.CategoryID).Return(&c, nil)
-	s.CategoriesRepo = mockCategoriesRepo
 
-	mcJSONBytes, err := json.Marshal(aToCreate)
+	mcJSONBytes, err := json.Marshal(aToUpdate)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, aToCreate.ID), bytes.NewBuffer(mcJSONBytes))
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, aToUpdate.ID), bytes.NewBuffer(mcJSONBytes))
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
@@ -373,25 +361,22 @@ func TestUpdateArticleAsReaderReturnForbidden(t *testing.T) {
 	ts := httptest.NewServer(s.Router)
 	defer ts.Close()
 
-	for _, c := range mockCategories {
-		s.CategoriesRepo.CreateCategory(&c)
-	}
-	for _, u := range mockUsers {
-		s.UsersRepo.CreateUser(&u)
-	}
-	for _, a := range mockArticles {
-		s.ArticlesRepo.CreateArticle(&a)
+	aToUpdate := models.Article{
+		ID:         0,
+		Title:      "First article",
+		CategoryID: 1,
+		UserID:     1,
 	}
 
-	mockArticle := mockArticles[1]
-	mockArticle.Title = "Article Updated"
+	mockArticlesRepo := &mocks.ArticlesRepository{}
+	s.ArticlesRepo = mockArticlesRepo
 
-	mcJSONBytes, err := json.Marshal(mockArticle)
+	mcJSONBytes, err := json.Marshal(aToUpdate)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, mockArticle.ID), bytes.NewBuffer(mcJSONBytes))
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, aToUpdate.ID), bytes.NewBuffer(mcJSONBytes))
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
@@ -418,26 +403,32 @@ func TestUpdateArticleAsWriterThatOwnsArticleReturnOk(t *testing.T) {
 	ts := httptest.NewServer(s.Router)
 	defer ts.Close()
 
-	mockArticles[1].UserID = 1
-	for _, c := range mockCategories {
-		s.CategoriesRepo.CreateCategory(&c)
-	}
-	for _, u := range mockUsers {
-		s.UsersRepo.CreateUser(&u)
-	}
-	for _, a := range mockArticles {
-		s.ArticlesRepo.CreateArticle(&a)
+	c := models.Category{
+		ID:   1,
+		Name: "First category",
 	}
 
-	mockArticle := mockArticles[1]
-	mockArticle.Title = "Article Updated"
+	aToUpdate := models.Article{
+		ID:         1,
+		Title:      "First article",
+		CategoryID: c.ID,
+		UserID:     1,
+	}
 
-	mcJSONBytes, err := json.Marshal(mockArticle)
+	aUpdated := aToUpdate
+	aUpdated.Title = "Article Updated"
+
+	mockArticlesRepo := &mocks.ArticlesRepository{}
+	mockArticlesRepo.On("GetArticle", aToUpdate.ID).Return(&aToUpdate, nil)
+	mockArticlesRepo.On("UpdateArticle", &aToUpdate).Return(&aUpdated, nil)
+	s.ArticlesRepo = mockArticlesRepo
+
+	mcJSONBytes, err := json.Marshal(aToUpdate)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, mockArticle.ID), bytes.NewBuffer(mcJSONBytes))
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, aToUpdate.ID), bytes.NewBuffer(mcJSONBytes))
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
@@ -459,31 +450,31 @@ func TestUpdateArticleAsWriterThatOwnsArticleReturnOk(t *testing.T) {
 	}
 }
 
-func TestUpdateArticleAsWriterThatDoesNotOwnArticleReturnOk(t *testing.T) {
+func TestUpdateArticleAsWriterThatDoesNotOwnArticleReturnForbidden(t *testing.T) {
 	s := NewTestServer()
 	ts := httptest.NewServer(s.Router)
 	defer ts.Close()
 
-	mockArticles[1].UserID = 2
-	for _, c := range mockCategories {
-		s.CategoriesRepo.CreateCategory(&c)
-	}
-	for _, u := range mockUsers {
-		s.UsersRepo.CreateUser(&u)
-	}
-	for _, a := range mockArticles {
-		s.ArticlesRepo.CreateArticle(&a)
+	aToUpdate := models.Article{
+		ID:         1,
+		Title:      "First article",
+		CategoryID: 1,
+		UserID:     2,
 	}
 
-	mockArticle := mockArticles[1]
-	mockArticle.Title = "Article Updated"
+	aUpdated := aToUpdate
+	aUpdated.Title = "Article Updated"
 
-	mcJSONBytes, err := json.Marshal(mockArticle)
+	mockArticlesRepo := &mocks.ArticlesRepository{}
+	mockArticlesRepo.On("GetArticle", aToUpdate.ID).Return(&aToUpdate, nil)
+	s.ArticlesRepo = mockArticlesRepo
+
+	mcJSONBytes, err := json.Marshal(aToUpdate)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, mockArticle.ID), bytes.NewBuffer(mcJSONBytes))
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, aToUpdate.ID), bytes.NewBuffer(mcJSONBytes))
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
@@ -510,26 +501,27 @@ func TestUpdateArticleAsAdministratorThatOwnsArticleReturnOk(t *testing.T) {
 	ts := httptest.NewServer(s.Router)
 	defer ts.Close()
 
-	mockArticles[1].UserID = 1
-	for _, c := range mockCategories {
-		s.CategoriesRepo.CreateCategory(&c)
-	}
-	for _, u := range mockUsers {
-		s.UsersRepo.CreateUser(&u)
-	}
-	for _, a := range mockArticles {
-		s.ArticlesRepo.CreateArticle(&a)
+	aToUpdate := models.Article{
+		ID:         1,
+		Title:      "First article",
+		CategoryID: 1,
+		UserID:     1,
 	}
 
-	mockArticle := mockArticles[1]
-	mockArticle.Title = "Article Updated"
+	aUpdated := aToUpdate
+	aUpdated.Title = "Article Updated"
 
-	mcJSONBytes, err := json.Marshal(mockArticle)
+	mockArticlesRepo := &mocks.ArticlesRepository{}
+	mockArticlesRepo.On("GetArticle", aToUpdate.ID).Return(&aToUpdate, nil)
+	mockArticlesRepo.On("UpdateArticle", &aToUpdate).Return(&aUpdated, nil)
+	s.ArticlesRepo = mockArticlesRepo
+
+	mcJSONBytes, err := json.Marshal(aToUpdate)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, mockArticle.ID), bytes.NewBuffer(mcJSONBytes))
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, aToUpdate.ID), bytes.NewBuffer(mcJSONBytes))
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
@@ -557,25 +549,27 @@ func TestUpdateArticleAsAdministratorThatDoesNotOwnArticleReturnOk(t *testing.T)
 	ts := httptest.NewServer(s.Router)
 	defer ts.Close()
 
-	for _, c := range mockCategories {
-		s.CategoriesRepo.CreateCategory(&c)
-	}
-	for _, u := range mockUsers {
-		s.UsersRepo.CreateUser(&u)
-	}
-	for _, a := range mockArticles {
-		s.ArticlesRepo.CreateArticle(&a)
+	aToUpdate := models.Article{
+		ID:         1,
+		Title:      "First article",
+		CategoryID: 1,
+		UserID:     2,
 	}
 
-	mockArticle := mockArticles[1]
-	mockArticle.Title = "Article Updated"
+	aUpdated := aToUpdate
+	aUpdated.Title = "Article Updated"
 
-	mcJSONBytes, err := json.Marshal(mockArticle)
+	mockArticlesRepo := &mocks.ArticlesRepository{}
+	mockArticlesRepo.On("GetArticle", aToUpdate.ID).Return(&aToUpdate, nil)
+	mockArticlesRepo.On("UpdateArticle", &aToUpdate).Return(&aUpdated, nil)
+	s.ArticlesRepo = mockArticlesRepo
+
+	mcJSONBytes, err := json.Marshal(aToUpdate)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, mockArticle.ID), bytes.NewBuffer(mcJSONBytes))
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/articles/%d", ts.URL, aToUpdate.ID), bytes.NewBuffer(mcJSONBytes))
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
